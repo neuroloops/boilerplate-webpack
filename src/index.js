e629acfedd1e57b6484db7a9ed9e532226de15f0
Ludo <ludoloops@protonmail.com>
2020-06-02 11:35:58 +0200

    Feat: favicon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './styles/styles.scss';
 import logoPath from './images/logo.svg';
+import favicon from './images/favicon.png';
 
 const version = 4;
 
@@ -7,3 +8,6 @@ document.getElementById('title').innerHTML = `bonjour de Webpack ${version}`;
 
 const logoImg = document.getElementById('logo');
 logoImg.src = logoPath;
+
+const faviconImg = document.getElementById('favicon');
+faviconImg.href = favicon;
